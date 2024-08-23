c2fe640dbaed30f95c5aa662cf9dc3df216f10f0
ackwolver335 <abhaych335@gmail.com>
2024-08-23 18:32:51 +0530

    Practice Set first for Day1 Completed

---
+++ Day1/ps1/practice.md
@@ -0,0 +1,11 @@
+## Practice Questions
+
+#### Q1. Create a variable of type string and try to add a number to it.
+
+#### Q2. Use typeof Operator to find the data type of the string in the last question.
+
+#### Q3. Create a const object in Javascrip. Can you change it to hold a number later ?
+
+#### Q4. Try to add a new key to the const object in Problem 3, were you able to do it ?
+
+#### Q5. Write a JS Problem to create a word-meaning dictionary of 5 words.
---
+++ Day1/ps1/setProgram.js
@@ -0,0 +1,32 @@
+// Chapter 1 - Q1.
+let str1 = "Ack Wolver";
+let num1 = 23;
+console.log(str1 + num1);
+
+// Chapter 1 - Q2.
+console.log(typeof str1);
+
+// Chapter 1 - Q3.
+const obj1 = {
+    new1 : "String1",
+    enrollment : 60
+}
+ 
+// obj1 = 23; We can't change to hold a number later
+
+// Chapter 1 - Q4.
+obj1["first"] = 34;
+console.log(obj1);
+
+// We are able to do it
+
+// Chapter 1 - Q5.
+const dict1 = {
+    "appreciate" : "to enjoy something or to understand the value of somebody/something",
+    "docile" : "quiet and easy to control",
+    "benchmark" : "a standard that other things can be compared to",
+    "doctorate" : "the highest university degree",
+    "nirvana" : "in Buddhism, the state of peace and happiness achieved after giving up personal desires"
+};
+
+console.log(dict1.docile);
