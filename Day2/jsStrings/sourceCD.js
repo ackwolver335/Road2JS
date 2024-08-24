a0f4cafd22080b23a6ac222fa9910985a7051f13
ackwolver335 <abhaych335@gmail.com>
2024-08-24 13:15:13 +0530

    Strings Concept Initiated

---
+++ Day2/jsStrings/sourceCD.js
@@ -0,0 +1,25 @@
+// Strings in JS
+// 1. Single Quoted Strings
+let single1 = 'Single Qouted';
+
+// 2. Double Quoted Strings
+let double1 = "String created with double Quotes !";
+
+// 3. Template Literals
+let firstName = "Ack";
+let lastName = "Wolver";
+
+console.log(`My First Name is : ${firstName} and Last Name is : ${lastName}`);
+
+// Escape Sequence Characters
+// 1. \' or \" for adding quotation in between of the Code
+console.log("Here we have \' quote with escape sequence character !");
+
+// 2. \n - Newline Characters
+console.log("First Line \nNew Line from here !");
+
+// 3. \t - Adding Tabspace Character
+console.log("Tabspace after \t here !");
+
+// 4. \r - Carriage Return
+console.log("Carriage Return after this \r here !");
