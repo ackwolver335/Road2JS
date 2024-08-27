57ff7f2ae8dfa034b1cfaa0411c287c9caa695bd
ackwolver335 <abhaych335@gmail.com>
2024-08-27 19:55:16 +0530

    Window Object Concept Added

---
+++ Day3/windowObj/script.js
@@ -0,0 +1,12 @@
+// Getting Data of Window Global Object
+console.log(window);
+
+// Some Alternatives
+window.console.log(window);
+console.log(document.body);                 // Getting the data of the body
+
+// Changing some properties of body with the use of JS
+document.body.style.background = "red";
+
+// getting towards the youtube page
+location.href = "https://youtube.com";
