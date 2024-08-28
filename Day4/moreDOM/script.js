02ce80771c8dcc962fefec2f412f03d8ac9032e1
ackwolver335 <abhaych335@gmail.com>
2024-08-28 10:21:34 +0530

    Day4 Initiated with DOM Object Model

---
+++ Day4/moreDOM/script.js
@@ -0,0 +1,16 @@
+// Categories of Nodes
+// 1. Text Node
+// 2. Element Node
+// 3. Comment Node
+
+// Getting the head details
+document.head;
+
+// Getting the title of the page
+document.title;
+
+// Changing the properties of the page
+document.body.style.background = 'cyan';
+
+// Accessing a particular element from the list of lot
+document.getElementsByClassName("contain1")[1].style.background = "grey";
