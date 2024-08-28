8a13a750291063afe22f1bb1846e87002e858502
ackwolver335 <abhaych335@gmail.com>
2024-08-28 20:15:12 +0530

    DOM Elements Searching Completed

---
+++ Day4/searchDOM/script.js
@@ -0,0 +1,15 @@
+// Changing the card title color to one
+// Getting the card
+let cardData = document.getElementsByClassName("card-title")[0];
+
+cardData.style.color = "red";
+
+/*
+  getElementById : Used for getting the element by the id but the properties are uniquely identified.
+  getElementByClassName : Used for applying the properties to a single class.
+  getElementsByClassName : Used for applying the properties to all the class using that particular classname.
+  querySelector : Used for applying properties or query to id and class at single basis.
+  querySelectorAll : Used for applying properties or query to id and classes basis.
+  getElementByTagName : Used for getting element using the tag name.
+  getElementByName : Used for getting the name using its value.
+*/
