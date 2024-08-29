646d0d956ecebd45c4e2b8158c9ceffe9c6283e9
ackwolver335 <abhaych335@gmail.com>
2024-08-29 11:22:33 +0530

    Exercise 2 Completed

--- Day3/jsExercise2/script.js
+++ Day3/jsExercise2/script.js
@@ -1,9 +1,48 @@
-// Getting the Value accessed from the random choices
-// creating array of values
-const choices = ['S','W','G'];
 
-let choice_method = () => {
-    const get_index = Math.floor(Math.random() * choices.length);
-    return choices[get_index];
-};
+// User's Choice
+const val1 = ['S','W','G'];
 
+// Getting a random choice
+let choice = () => {
+    let choice1 = Math.floor((Math.random()) * 3);
+    return val1[choice1];
+}
+
+let chances = 0;
+let score = 10;
+
+while(chances < 10){
+
+    let userChoice = prompt("Enter from 'S' | 'W' | 'G' : ");
+    let comptr_choice = choice();
+
+    if(userChoice == 'W' && comptr_choice == 'S'){
+        score--;
+        alert("You Lose !");
+    }
+
+    else if(userChoice == 'S' && comptr_choice == 'G'){
+        score--;
+        alert('You Lose !');
+    }
+
+    else if(userChoice == 'G' && comptr_choice == 'W'){
+        score--;
+        alert('You Lose !');
+    }
+
+    else if(userChoice == comptr_choice){
+        alert('Draw !');
+    }
+
+    else{
+        if(score < 10){
+            score++;
+        }
+        alert('You Won !');
+    }
+
+    chances++;
+}
+
+console.log("You score is : " + score);
