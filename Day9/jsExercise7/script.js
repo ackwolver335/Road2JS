c66051097fa35fa82f9b0f760d3e49fe5f9f93bc
ackwolver335 <abhaych335@gmail.com>
2024-09-10 08:04:58 +0530

    JS Exercise7 Completed

--- Day9/jsExercise7/script.js
+++ Day9/jsExercise7/script.js
@@ -2,3 +2,39 @@
     Create a Password Generator Program capable of generating a password which contains atleast one lowercase, one uppercase adn one 
     special case character. Create a password class to achieve the same.
 */
+
+class Password{
+    constructor(){
+        console.log("Password Generator Initiated !");
+        this.passWord = "";
+    }
+
+    generatePass(len){
+        
+        // List of values to be used in passwords
+        let alpha1 = "abcdefghijklmnopqrstuvwxyz";
+        let numericalVal = "01234546789";
+        let symbols = "~`!@#$%^&*()_+-=:',.<>/?|[]{}";
+
+        if(len < 3){
+            console.log("Your password must contains more than 5 characters !");
+            return;
+        }
+
+        else{
+            let i = 0;
+            while(i < len){
+                this.passWord += alpha1[Math.floor(Math.random(alpha1.lenght) * alpha1.length)];
+                this.passWord += numericalVal[Math.floor(Math.random(alpha1.lenght) * numericalVal.length)];
+                this.passWord += symbols[Math.floor(Math.random(alpha1.lenght) * symbols.length)];
+
+                i += 3;
+            }
+            this.passWord = this.passWord.substring(0,len);
+            return this.passWord;
+        }
+    }
+}
+
+let obj1 = new Password();
+console.log(obj1.generatePass(9));
