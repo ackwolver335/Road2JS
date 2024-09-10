1e8c20a1ec6da6e63dc145e22168a97f62770378
ackwolver335 <abhaych335@gmail.com>
2024-09-10 08:19:49 +0530

    Intro to Node JS

---
+++ Day10/jsonServer/script.js
@@ -0,0 +1,30 @@
+// JS On Server
+/*
+    Welcome to Node.js v22.7.0.
+    Type ".help" for more information.
+    > console.log("Hello World");
+    Hello World
+    undefined
+    > let b = 56;
+    undefined
+    > let c = 55;
+    undefined
+    > console.log(b,c);
+    56 55
+    undefined
+    > 
+    (To exit, press Ctrl+C again or Ctrl+D or type .exit)
+    >
+
+    Welcome to Node.js v22.7.0.
+    Type ".help" for more information.
+    > 
+    (To exit, press Ctrl+C again or Ctrl+D or type .exit)
+    > 
+    ackwolver@Dell-Latitude-E6420:~$ node --version
+    v22.7.0
+    ackwolver@Dell-Latitude-E6420:~$ npm --version
+    10.8.2
+    ackwolver@Dell-Latitude-E6420:~$ nvm --version
+    0.40.0
+*/
